051618a6373fc5f1c89724984a70bfdcb276769a
Karla Santos <karla_s@dell.com>
2025-06-16 06:12:04 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def retrieve_names():
+    """ The readlines() method returns a list containing each line in the file as a list item """
+    with open ("Input/Names/invited_names.txt") as names_file:
+        return names_file.readlines()
+
+def retrieve_letter():
+    """ The read() method returns a string with the contents of the file """
+    with open ("Input/Letters/starting_letter.txt") as letter_file:
+        return letter_file.read()
+
+def create_file(guest_name):
+    new_letter = original_letter.replace("[name]", guest_name)
+    with open(f"./Output/ReadyToSend/letter_for_{guest_name}.txt", mode="a") as file:
+        file.write(new_letter)
+
+original_letter = retrieve_letter()
+names = retrieve_names()
+names.sort()
+
+for name in names:
+    """ The strip() method removes any whitespace from the beginning or the end """
+    print(f"Creating letter for {name}...")
+    create_file(name.strip())
